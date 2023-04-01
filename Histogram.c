26a399785ed983c6615faa8ac29eb59a920d6b6f
Amir Kalaki <115138312+KLX1899@users.noreply.github.com>
2023-04-01 17:08:06 +0330

    Add files via upload

---
+++ Histogram.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void Histogram(char text[]) {
+    
+    for (int i = 0 ; i < strlen(text) ; i++) {
+        int number = 0;
+        char temp = text[i];
+        for (int counter = 0 ; counter < strlen(text) ; counter++) {
+                if (temp == text[counter]) {
+                    number++;
+                }
+        }
+        printf("\'%c\' | " , text[i]);
+        for (int j = 0 ; j < number ; j++) {
+            printf("X");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    
+    char text[] = "hello, world!";
+    printf("----------------------------------------------------------------\n");
+    Histogram(text);
+    printf("----------------------------------------------------------------");
+
+    return 0;
+}
