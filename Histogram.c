f13e8f0372851580d1faadb1e0bb51112cb60ec5
KLX1899 <amirm.kalaki@gmail.com>
2023-09-16 18:32:11 +0330

    Minor changes in display

--- Histogram.c
+++ Histogram.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 void Histogram(char text[]) {
     
     for (int i = 0 ; i < strlen(text) ; i++) {
@@ -19,14 +21,17 @@ void Histogram(char text[]) {
     }
 }
 
-int main() {
+
+
+void main() {
     
     char text[100];
 
+    printf("\t\t\" HISTOGRAM \"");
+    printf("Write a text (100 character)\n> ");
     fgets(text , 100 , stdin);
     printf("----------------------------------------------------------------\n");
     Histogram(text);
     printf("----------------------------------------------------------------");
 
-    return 0;
 }
